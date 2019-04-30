097ec6d2c18cf82218c7a1b10714e3d1dedb1c7d
danilo <danilosantosalvarado@gmail.com>
2019-04-30 18:01:15 -0500

    Initial commit

---
+++ sites/all/themes/custom/serempre/template.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * The primary PHP file for the Drupal Bootstrap base theme.
+ */
+
+ function andes_preprocess_page(&$vars, $hook) {
+    if (true) {
+      // drupal_add_js(drupal_get_path('theme', 'andes') . '/js/script.js');
+      // $vars['scripts'] = drupal_get_js();
+    }
+  }
