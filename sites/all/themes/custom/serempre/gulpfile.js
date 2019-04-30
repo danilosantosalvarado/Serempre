64cc132230d42f7f5fdd4905a8c2b495ce25e728
danilo <danilosantosalvarado@gmail.com>
2019-04-30 18:18:24 -0500

    node_modules

--- sites/all/themes/custom/serempre/gulpfile.js
+++ sites/all/themes/custom/serempre/gulpfile.js
@@ -16,7 +16,7 @@ var options = {};
 // accordingly.
 options.rootPath = {
   project     : __dirname + '/',
-  styleGuide  : __dirname + '/andes/',
+  styleGuide  : __dirname + '/serempre/',
   theme       : __dirname + '/'
 };
 
@@ -30,7 +30,7 @@ options.theme = {
 // Set the URL used to access the Drupal website under development. This will
 // allow Browser Sync to serve the website and update CSS changes on the fly.
 //options.drupalURL = 'http://andes.dev.local/';
- options.drupalURL = 'http://andes.dev.local/';
+ options.drupalURL = 'http://localhost/serempre/';
 
 // Define the node-sass configuration. The includePaths is critical!
 options.sass = {
@@ -180,7 +180,7 @@ gulp.task('lint:sass-with-fail', function() {
 // ##############################
 // Watch for changes and rebuild.
 // ##############################
-gulp.task('watch', ['browser-sync','watch:js']); //,'watch:lint-and-styleguide'
+gulp.task('watch', ['browser-sync']); //,'watch:lint-and-styleguide'
 
 gulp.task('browser-sync', ['watch:css'], function() {
   if (!options.drupalURL) {
